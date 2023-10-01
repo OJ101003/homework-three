61350ebe613b890b1e18d9edd5f4f1b9fe0315e3
OJ101003 <hackerdude1010@gmail.com>
2023-10-01 14:31:55 -0700

    Cleaned the code

--- calculator.js
+++ calculator.js
@@ -1,63 +1,66 @@
 const numbersDiv = document.querySelector("#numbers p");
-const numBtn = document.querySelectorAll(".numbutton");
-const calcBtn = document.querySelectorAll(".calcbutton");
+const numButtons = document.querySelectorAll(".numbutton");
+const calcButtons = document.querySelectorAll(".calcbutton");
 
-let mathStatus = true;
-let firstNum;
-let secondNum = "";
-let symbol = "";
-numBtn.forEach((button) => {
+let isFirstNumDone = true;
+let firstNumber;
+let secondNumber = "";
+let operator = "";
+
+numButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    nums = event.currentTarget.textContent;
-    numbersDiv.textContent += nums;
-    if (!mathStatus){
-        secondNum += nums;
+    const number = event.currentTarget.textContent;
+    numbersDiv.textContent += number;
+    if (!isFirstNumDone) {
+      secondNumber += number;
     }
   });
 });
 
-calcBtn.forEach((button) => {
+calcButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    if (mathStatus){
-        firstNum = numbersDiv.textContent;
-        numbersDiv.textContent += ` ${event.currentTarget.textContent} `;
-        mathStatus = false;
+    if (isFirstNumDone) {
+      firstNumber = numbersDiv.textContent;
+      numbersDiv.textContent += ` ${event.currentTarget.textContent} `;
+      isFirstNumDone = false;
+    } else {
+      numbersDiv.textContent = `${firstNumber} ${event.currentTarget.textContent} ${secondNumber}`;
     }
-    else{
-        numbersDiv.textContent = `${firstNum} ${event.currentTarget.textContent} ${secondNum}`
-    }
-    symbol = event.currentTarget.textContent;
+    operator = event.currentTarget.textContent;
   });
 });
 
-const resetBtn = () => {
-    numbersDiv.textContent = ""
-    mathStatus = true;
-    secondNum = "";
+const resetCalculator = () => {
+  numbersDiv.textContent = "";
+  isFirstNumDone = true;
+  secondNumber = "";
 };
 
-const sumBtn = () =>{
-    if (mathStatus){
-        return;
-    }
-    let first = parseInt(firstNum);
-    let second = parseInt(secondNum);
-    let result;
-    switch(symbol) {
-        case "+":
-            result = first+second;
-            break;
-        case "-":
-            result = first-second;
-            break;
-        case "/":
-            result = Math.round((first/second) * 100) / 100;
-            break;
-        case "X":
-            result = (first*second)
-            break;
-    }
-    numbersDiv.textContent = result;
-    mathStatus = true;
-    secondNum = "";
+const performCalculation = () => {
+  if (isFirstNumDone) {
+    return;
+  }
+
+  const parsedFirstNumber = parseFloat(firstNumber);
+  const parsedSecondNumber = parseFloat(secondNumber);
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = parsedFirstNumber + parsedSecondNumber;
+      break;
+    case "-":
+      result = parsedFirstNumber - parsedSecondNumber;
+      break;
+    case "/":
+      result = Math.round((parsedFirstNumber / parsedSecondNumber) * 100) / 100;
+      break;
+    case "X":
+      result = parsedFirstNumber * parsedSecondNumber;
+      break;
+  }
+
+  numbersDiv.textContent = result;
+  isFirstNumDone = true;
+  secondNumber = "";
 };
