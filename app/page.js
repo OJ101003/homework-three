54f71d4f9577bbba85ab22408ab9381131ae94bd
OJ101003 <hackerdude1010@gmail.com>
2023-10-30 17:19:58 -0700

    dawda

--- app/page.js
+++ app/page.js
@@ -1,17 +1,28 @@
+// ./app\page.js
+"use client";
 import CalcBTN from "@/components/CalcBTN";
+import NumField from "@/components/NumField";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [equation, setEquation] = useState("")
+
+  const appendSymbols = (val) => {
+    setEquation(...equation, val.toString())
+    console.log(equation)
+  }
   return (
     <div className="flex justify-center items-center">
       <div className="w-[380px] h-[500px] m-[5px] rounded-xl bg-slate-300 ">
         <div className="py-[35px] px-[50px] m-[5%] rounded-[20px] border-[5px] border-solid border-white h-0 flex justify-center items-center">
-          <p className="m-0 p-0 text-[30px]"></p>
+          <NumField value={""} />
         </div>
         <div className="flex flex-row justify-center flex-wrap bg-slate-200 pt-[10px]">
           {[7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "X", "C", 0, "=", "/"].map(
             (value) => (
-              <CalcBTN key={value} value={value}></CalcBTN>
+              <CalcBTN key={value} value={value} onClick={appendSymbols}></CalcBTN>
             )
           )}
         </div>
--- components/CalcBTN.js
+++ components/CalcBTN.js
@@ -1,6 +1,6 @@
 import { Button } from "./ui/button";
-export default function CalcBTN({ value }) {
+export default function CalcBTN({ value, onClick }) {
   return (
-    <Button className="w-[70px] h-[70px] font-bold text-[30px] m-[10px]">{value}</Button>
+    <Button className="w-[70px] h-[70px] font-bold text-[30px] m-[10px]" onClick={() => onClick(value)}>{value}</Button>
   );
 }
