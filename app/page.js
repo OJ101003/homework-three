04cdd482650a573f3809a17fc3777b13223d8b04
OJ101003 <hackerdude1010@gmail.com>
2023-11-04 20:44:15 -0700

    Finished

--- app/page.js
+++ app/page.js
@@ -5,25 +5,51 @@ import NumField from "@/components/NumField";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
-
 export default function Home() {
-  const [equation, setEquation] = useState("")
+  const [equation, setEquation] = useState("");
 
   const appendSymbols = (val) => {
-    setEquation(...equation, val.toString())
-    console.log(equation)
-  }
+    if (val === "C") {
+      setEquation("");
+      return;
+    } 
+    setEquation(equation + val.toString());
+    console.log(equation, typeof val);
+  };
+  const updateText = () => {
+    try {
+      setEquation(eval(equation))
+    } catch (err) {
+      setEquation("ERROR")
+    }
+  };
   return (
     <div className="flex justify-center items-center">
       <div className="w-[380px] h-[500px] m-[5px] rounded-xl bg-slate-300 ">
         <div className="py-[35px] px-[50px] m-[5%] rounded-[20px] border-[5px] border-solid border-white h-0 flex justify-center items-center">
-          <NumField value={""} />
+          <NumField value={equation} />
         </div>
         <div className="flex flex-row justify-center flex-wrap bg-slate-200 pt-[10px]">
           {[7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "X", "C", 0, "=", "/"].map(
-            (value) => (
-              <CalcBTN key={value} value={value} onClick={appendSymbols}></CalcBTN>
-            )
+            (value) => {
+              if (value === "=") {
+                return (
+                  <CalcBTN
+                    key={value}
+                    value={value}
+                    onClick={updateText}
+                  ></CalcBTN>
+                );
+              } else {
+                return (
+                  <CalcBTN
+                    key={value}
+                    value={value}
+                    onClick={appendSymbols}
+                  ></CalcBTN>
+                );
+              }
+            }
           )}
         </div>
       </div>
--- components/NumField.js
+++ components/NumField.js
@@ -1,4 +1,5 @@
 export default function NumField({ value }) {
+    
     return (
         <p className="m-0 p-0 text-[30px]">{value}</p>
     )
